8e94d3e456b38be9f9f588e10d98d5af5018c2ca
Frederick Gómez <fgomez@singularagency.co>
2025-03-27 12:40:48 -0400

    Implement unread email retrieval and add folder fetching functionality in ZohoEmailAPI

--- app/api/v1/endpoints/invoices.py
+++ app/api/v1/endpoints/invoices.py
@@ -5,9 +5,12 @@ from app.clients.zoho_client import ZohoEmailAPI
 router = APIRouter(prefix="/v1/invoices", tags=["invoices"])
 
 emailProcessor = ZohoEmailAPI()
+emailProcessor.connect()
 
 
 @router.get("/unread")
 async def get_unread_emails():
     """Get unread emails"""
-    return {"message": "Get unread emails"}
+    
+    emails = emailProcessor.get_unread_messages()
+    return emails
