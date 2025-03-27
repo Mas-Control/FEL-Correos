98e28c9d38ceabef940dbdc02d536f21503a04e2
Frederick Gómez <fgomez@singularagency.co>
2025-03-27 13:05:11 -0400

    Establish connection to ZohoEmailAPI in get_unread_emails endpoint

--- app/api/v1/endpoints/invoices.py
+++ app/api/v1/endpoints/invoices.py
@@ -5,12 +5,12 @@ from app.clients.zoho_client import ZohoEmailAPI
 router = APIRouter(prefix="/v1/invoices", tags=["invoices"])
 
 emailProcessor = ZohoEmailAPI()
-emailProcessor.connect()
 
 
 @router.get("/unread")
 async def get_unread_emails():
     """Get unread emails"""
     
+    emailProcessor.connect()
     emails = emailProcessor.get_unread_messages()
     return emails
